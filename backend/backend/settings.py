508d41c7c9d2dcd225a83b8eb70898e1fbcb5875
mireiatz <taboadamireia@gmail.com>
2025-05-08 20:43:23 +0200

    Main setup
    - Fixed corsheaders middleware import in settings
    - Modified backend dockerfile

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.corsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
