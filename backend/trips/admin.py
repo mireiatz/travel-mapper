3d0a14cac07c33f688ec9f2e66e8df3894947820
mireiatz <taboadamireia@gmail.com>
2025-05-31 20:39:18 +0200

    Admin set up
    - Registered trip and journey in admin
    - Created dockerignore

--- backend/trips/admin.py
+++ backend/trips/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Trip, Journey
 
-# Register your models here.
+@admin.register(Trip)
+class TripAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'user', 'created_at')
+    search_fields = ('name', 'user__username')
+    list_filter = ('start_date', 'end_date')
+
+@admin.register(Journey)
+class JourneyAdmin(admin.ModelAdmin):
+    list_display = ('from_location_name', 'from_location_lat', 'from_location_lng', 'to_location_name', 'to_location_lat', 'to_location_lng', 'transport_type', 'trip', 'order')
+    list_filter = ('transport_type', 'trip')
+    search_fields = ('from_location_name', 'to_location_name')
